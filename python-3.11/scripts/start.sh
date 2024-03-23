94a3033fc2940727f0e197f90c849d79c2649af9
Boro Vukovic <5162876+TheBoroer@users.noreply.github.com>
2024-03-23 04:46:27 -0400

    Update start.sh

--- python-3.11/scripts/start.sh
+++ python-3.11/scripts/start.sh
@@ -48,13 +48,12 @@ else
  fi
 fi
 
-# Install python deps via pipenv
-if [ -f "$WORKDIR/Pipfile" ] ; then
-  cd $WORKDIR && pipenv install && echo "Python packages installed from Pipfile"
-fi
-# Install python deps via pip
 if [ -f "$WORKDIR/requirements.txt" ] ; then
+  # Install python deps via pip
   cd $WORKDIR && pip install --no-cache-dir -r requirements.txt && echo "Python packages installed from requirements.txt"
+elif [ -f "$WORKDIR/Pipfile" ] ; then
+  # Otherwise install python deps via pipenv
+  cd $WORKDIR && pipenv install && echo "Python packages installed from Pipfile"
 fi
 
 
